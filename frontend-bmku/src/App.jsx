82fe79921db4526a12fe36e6a552107756a7dae3
darrenchrist2 <darrensiwibu@gmail.com>
2026-07-09 10:48:39 +0700

    edit app jsx

--- frontend-bmku/src/App.jsx
+++ frontend-bmku/src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Sidebar from './components/sidebar';
-import '../pages/inventoryItems';
 
 /**
  * Contoh integrasi Sidebar ke dalam layout aplikasi.
